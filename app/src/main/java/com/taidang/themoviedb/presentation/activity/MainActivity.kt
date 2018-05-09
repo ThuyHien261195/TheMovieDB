8651cb2f49c15cb024f78527c890547fbbead140
ThuyHien261195 <thuyhien261195@gmail.com>
2018-05-09 16:51:16 +0700

    Update main activity with two bottom tab

--- app/src/main/java/com/taidang/themoviedb/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/taidang/themoviedb/presentation/activity/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.taidang.themoviedb.presentation.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.FragmentTransaction
 import com.taidang.themoviedb.R
-import com.taidang.themoviedb.presentation.adapter.MainContentPagerAdapter
-import com.taidang.themoviedb.presentation.adapter.ZoomOutPageTransformer
+import com.taidang.themoviedb.extension.enum.FragmentType
+import com.taidang.themoviedb.presentation.fragment.MovieFragment
+import com.taidang.themoviedb.presentation.fragment.TvShowFragment
+import com.taidang.themoviedb.presentation.listener.PageSelectedListener
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.support.v4.onPageChangeListener
+import kotlinx.android.synthetic.main.include_tab_media.*
 
-class MainActivity : DaggerAppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity(), PageSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainContentPagerAdapter(this, supportFragmentManager)
-        vMainPager.adapter = adapter
-        vMainPager.setPageTransformer(true, ZoomOutPageTransformer())
-        vMainPager.onPageChangeListener {
-            onPageSelected { vToolbar.title = adapter.getPageTitle(it) }
+        btnMovie.setOnClickListener{
+            showFragment(FragmentType.MOVIE)
         }
 
-        vToolbar.title = adapter.getPageTitle(vMainPager.currentItem)
+        btnSeries.setOnClickListener{
+            showFragment(FragmentType.TVSHOWS)
+        }
+
+        showFragment(FragmentType.MOVIE)
+    }
+
+    private fun showFragment(fragmentType: FragmentType) {
+        val fragmentTransition : FragmentTransaction = supportFragmentManager.beginTransaction()
+        when(fragmentType) {
+            FragmentType.MOVIE -> fragmentTransition.replace(R.id.frameLayoutMain, MovieFragment())
+            FragmentType.TVSHOWS -> fragmentTransition.replace(R.id.frameLayoutMain, TvShowFragment())
+            else -> fragmentTransition.replace(R.id.frameLayoutMain, MovieFragment())
+        }
+        fragmentTransition.commit()
+    }
+
+    override fun onPageSelected(title: String) {
+        vToolbar.title = title
     }
 }
