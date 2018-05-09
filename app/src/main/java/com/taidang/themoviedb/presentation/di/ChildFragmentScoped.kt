8039357a6530458fbece398ec7fa89dc5dfb1e29
ThuyHien261195 <thuyhien261195@gmail.com>
2018-05-09 16:53:15 +0700

    Add child fragment scoped

---
+++ app/src/main/java/com/taidang/themoviedb/presentation/di/ChildFragmentScoped.kt
@@ -0,0 +1,11 @@
+package com.taidang.themoviedb.presentation.di
+
+import javax.inject.Scope
+
+/**
+ * Created by thuyhien on 5/3/18.
+ */
+@Scope
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ChildFragmentScoped
