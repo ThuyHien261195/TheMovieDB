4d5e5bbf7f333644b4204eaf6d990a7b016bbfb5
ThuyHien261195 <thuyhien261195@gmail.com>
2018-05-15 17:09:37 +0700

    Add extension to config for common horizontal recycler view

--- app/src/main/java/com/taidang/themoviedb/extension/ViewExtension.kt
+++ app/src/main/java/com/taidang/themoviedb/extension/ViewExtension.kt
@@ -1,9 +1,15 @@
 package com.taidang.themoviedb.extension
 
+import android.content.Context
 import android.support.annotation.LayoutRes
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.taidang.themoviedb.R
 
 fun View.visible() {
     this.visibility = View.VISIBLE
@@ -21,4 +27,15 @@ fun View.isShowing() = this.visibility == View.VISIBLE
 
 fun ViewGroup.inflate(@LayoutRes layoutResId: Int): View {
     return LayoutInflater.from(context).inflate(layoutResId, this, false)
+}
+
+fun RecyclerView.configHorizontalListView(context: Context) {
+    this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    this.setHasFixedSize(true)
+
+    ContextCompat.getDrawable(context, R.drawable.cast_listing_divider)?.let {
+        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
+        dividerItemDecoration.setDrawable(it)
+        this.addItemDecoration(dividerItemDecoration)
+    }
 }
